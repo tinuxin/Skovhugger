10b3aa66ea69c568edc223d383ba7a3d0c962ea6
tinus <tinusabell@gmail.com>
2015-02-09 23:09:18 +0100

    Changed submit button Added Cancel button Reverse teams on new game

--- controllers/view2Controller.js
+++ controllers/view2Controller.js
@@ -6,31 +6,24 @@ skovhuggerApp.controller('View2Controller', function ($scope, Data, $route) {
 
     $scope.roundCounter = 0;
     $scope.activeTeamCounter = 0;
-    $scope.scorePreviousTargetRow = $scope.scoreTable[0];
-    $scope.scoreActiveTargetRow = $scope.scoreTable[0];
 
     $scope.hits=null;
-    $scope.value = $scope.scoreActiveTargetRow.value;
-    $scope.multiplier = $scope.scoreActiveTargetRow.multiplier;
+    $scope.value = function() {return $scope.getScoreTargetRow().value;}
     $scope.multiValue = Array(3);
     $scope.gameover = false;
 
     $scope.addScore = function () {
-        $scope.scoreActiveTargetRow.score[$scope.activeTeamCounter] = $scope.calculateNewTotalScore();
-        $scope.activeTeamCounter++;
+        if ($scope.checkValid()) {
+            $scope.getScoreTargetRow().score[$scope.activeTeamCounter] = $scope.calculateNewTotalScore();
+            $scope.activeTeamCounter++;
 
-        if ($scope.numberOfTeams === $scope.activeTeamCounter) {
-            $scope.roundCounter++;
-            if ($scope.roundCounter >= $scope.scoreTable.length) {
-                $scope.gameover=true;
-            } else {
-            $scope.scorePreviousTargetRow = $scope.scoreActiveTargetRow;
-            $scope.scoreActiveTargetRow = $scope.scoreTable[$scope.roundCounter];
-
-            $scope.value = $scope.scoreActiveTargetRow.value;
-            $scope.multiplier = $scope.scoreActiveTargetRow.multiplier;
-
-            $scope.activeTeamCounter = 0;
+            if ($scope.numberOfTeams === $scope.activeTeamCounter) {
+                $scope.roundCounter++;
+                if ($scope.roundCounter >= $scope.scoreTable.length) {
+                    $scope.gameover = true;
+                } else {
+                    $scope.activeTeamCounter = 0;
+                }
             }
         }
 
@@ -39,29 +32,40 @@ skovhuggerApp.controller('View2Controller', function ($scope, Data, $route) {
     }
 
     $scope.calculateNewTotalScore = function () {
-        var score = 0, previousScore = $scope.roundCounter === 0 ? 0 : $scope.scorePreviousTargetRow.score[$scope.activeTeamCounter];
+        var score = 0, previousScore = $scope.roundCounter === 0 ? 0 : $scope.getPreviousScoreTargetRow().score[$scope.activeTeamCounter];
 
         if (!$scope.hits) {
             return Math.floor(previousScore / 2);
         }
 
-        if ($scope.value === null) {
+        if ($scope.value() === null) {
             for (var i = 0; i < $scope.hits; i++) {
-                score = score + $scope.multiValue[i] * $scope.multiplier;
+                score = score + $scope.multiValue[i] * $scope.getScoreTargetRow().multiplier;
             }
         } else {
-            score = $scope.value * $scope.hits;
+            score = $scope.value() * $scope.hits;
         }
 
         return previousScore + score;
     }
 
+    $scope.getScoreTargetRow = function() {
+        return $scope.getScoreRow($scope.roundCounter);
+    }
+
+    $scope.getPreviousScoreTargetRow = function() {
+        return $scope.getScoreRow($scope.roundCounter-1);
+    }
+
+    $scope.getScoreRow = function(round) {
+        return $scope.scoreTable[round];
+    }
 
     $scope.checkValid = function () {
         if ($scope.hits == null) {
             return false;
         }
-        if ($scope.value === null) {
+        if ($scope.value() === null) {
             for (var i = 0; i < $scope.hits; i++) {
                 if (!$scope.multiValue[i]) {
                     return false;
@@ -71,28 +75,23 @@ skovhuggerApp.controller('View2Controller', function ($scope, Data, $route) {
         return true;
     }
 
+    $scope.cancelScore = function() {
+        if ($scope.activeTeamCounter === 0) {
+            $scope.activeTeamCounter = $scope.numberOfTeams-1;
+            $scope.roundCounter--;
+        } else {
+            $scope.activeTeamCounter--;
+        }
+        $scope.getScoreTargetRow().score.splice($scope.activeTeamCounter, 1);
+    }
+
     $scope.newGame = function () {
         Data.resetScore();
 
         $scope.roundCounter = 0;
         $scope.activeTeamCounter = 0;
-        $scope.scorePreviousTargetRow = $scope.scoreTable[0];
-        $scope.scoreActiveTargetRow = $scope.scoreTable[0];
 
-        $scope.value = $scope.scoreActiveTargetRow.value;
-        $scope.multiplier = $scope.scoreActiveTargetRow.multiplier;
         $scope.gameover = false;
-    }
-
-    $scope.newScore;
-    $scope.showModal = function (targetRow, teamIndex) {
-        $scope.newScore = null;
-        $('#editValueModal').on('hide.bs.modal',function (e) {
-            targetRow.score[teamIndex] = $scope.newScore;
-        }).modal('show');
-    }
-
-    $scope.hideModal = function () {
-        $('#editValueModal').modal('hide').off('hide.bs.modal');
+        Data.teams.reverse();
     }
 });
